3357e6256aa8501e846f01dfe1d60e534a590826
leopoglia <92129999+leopoglia@users.noreply.github.com>
2021-12-08 11:25:07 -0300

    Add files via upload

--- index/mode.js
+++ index/mode.js
@@ -1,24 +1,34 @@
-function pedro(){
+function pedro() {
     document.querySelector('a').style = 'color: #154bbb;';
     document.body.style = "background-color: #e7e7e7;";
-    nav.style.backgroundColor = "white";
+    nav.style = "background-color: white;";
 
     document.getElementById("a").style.color = "#154bbb";
     document.getElementById("b").style.color = "#154bbb";
     document.getElementById("c").style.color = "#154bbb";
-    document.getElementById("menu-container").style = "background-image: linear-gradient(to top, #00c6fb 0%, #005bea 100%);";
+    document.getElementById("menu-container").style = "background-color: #154bbb";
     document.getElementById("container").style.background = "white";
     document.getElementById("input-menu").style = "background-color: white; box-shadow: 0px -4px 0px rgb(225,225,225) inset;";
     document.getElementById("search").style = "color: #2a2a2a;";
     document.getElementById("mode").style = "color: #154bbb;";
     document.getElementById("login").style = "color: #154bbb;";
     document.getElementById("version").style = "background: #e7e7e7; color: #154bbb;";
+    document.getElementById("titulo-pagina").style = "background-color: #e7e7e7;";
+    document.getElementById("casa").style = "color: #154bbb;";
+    var navlist = document.querySelectorAll('.nav-list');
+    navlist[0].style = "background-color: white; box-shadow: 0px 0px 0px #154bbb";
+
+    var tracos = document.querySelectorAll('.mobile-menu div');
+    tracos[0].style = "background: #154bbb;";
+    tracos[1].style = "background: #154bbb;";
+    tracos[2].style = "background: #154bbb;";
+
 
     var active = document.querySelectorAll('.jogos');
     var active2 = document.querySelectorAll('.titulojogo');
-    for(var i = 0; i < active.length; i++) {
-        active[i].style = "background-image: linear-gradient(90deg, #00c6fb 0%, #005bea 100%); box-shadow: 0px 0px 0px transparent;";
-        active2[i].style = "background-image: linear-gradient(90deg, #00c6fb 0%, #005bea 100%); border-radius: 0px";
+    for (var i = 0; i < active.length; i++) {
+        active[i].style = "background-color: #154bbb";
+        active2[i].style = "background-color: #154bbb";
 
     }
 }
