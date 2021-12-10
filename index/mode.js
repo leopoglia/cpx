21e491466524d0319ae674f18b7ade9111ea9bd9
leopoglia <92129999+leopoglia@users.noreply.github.com>
2021-12-09 23:48:39 -0300

    Add files via upload

--- index/mode.js
+++ index/mode.js
@@ -5,5 +5,32 @@ function myFunction() {
     navligth.classList.toggle("dark-mode");
     var logona = document.getElementById("logo");
     logona.classList.toggle("dark-mode");
-
+    var a = document.getElementById("a");
+    a.classList.toggle("dark-mode");
+    var b = document.getElementById("b");
+    b.classList.toggle("dark-mode");
+    var c = document.getElementById("c");
+    c.classList.toggle("dark-mode");
+    var modee = document.getElementById("mode");
+    modee.classList.toggle("dark-mode");
+    var login = document.getElementById("login");
+    login.classList.toggle("dark-mode");
+    var menu = document.getElementById("menu-container");
+    menu.classList.toggle("dark-mode2");
+    var titulo = document.getElementById("titulo-pagina");
+    titulo.classList.toggle("dark-mode3");
+    var input = document.getElementById("input-menu");
+    input.classList.toggle("dark-mode3");
+    var search = document.getElementById("search");
+    search.classList.toggle("dark-mode");
+    var container = document.getElementById("container");
+    container.classList.toggle("dark-mode");
+    var list =  document.querySelector(".nav-list");
+    list.classList.toggle("dark-mode");
+    var mobilemenu =  document.querySelector(".mobile-menu");
+    mobilemenu.classList.toggle("dark-mode2");
+    var valores = document.getElementsByClassName('jogos');
+for (var i=0; valores[i]; i++) {
+    valores[i].classList.toggle("dark-mode2");
+}
 }
